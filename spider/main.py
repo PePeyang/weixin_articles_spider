c59d55fa59adb8d458cca0cd36eb643625e79c89
= <sotyoyo@icloud.com>
2019-08-24 16:25:40 +0800

    add 小流程

--- spider/main.py
+++ spider/main.py
@@ -43,8 +43,8 @@ class Spider:
         # /usr/bin/chromedriver
         chrome = input('请输入chromedriver路径 (注意要和自己的chrome版本匹配):').strip()
         if not chrome:
-            if os.path.isfile('chromedriver'):
-                chrome = 'chromedriver'
+            if os.path.isfile('/usr/bin/chromedriver'):
+                chrome = '/usr/bin/chromedriver'
             else:
                 print('没有合适的chromedriver，程序无法进行！')
                 return
@@ -60,24 +60,38 @@ class Spider:
         if not driver:
             print('Are you kidding me ？没有driver玩毛线')
             return
-
-        cookies = json.load(open('caches/cookies.json', 'rb')
-                            ) if os.path.isfile('caches/cookies.json') else []
+        base_dir = os.path.dirname(__file__)
+        cookies_path = os.path.join(base_dir, 'caches/cookies.json')
+        cookies = json.load(open(cookies_path, 'rb')
+                            ) if os.path.isfile(cookies_path) else []
         driver.get(Urls.index)
         if not cookies:
             # TODO 自动输入账号密码 给出二维码然后自动登陆 只需要自己扫个码
             # ANCHOR cookie一般一天就到期
             input("请先手动登录, 完成后按回车继续:")
             cookies = driver.get_cookies()
-            open('caches/cookies.json', 'wb').write(json.dumps(cookies).encode('utf-8'))
+            open(cookies_path, 'wb').write(json.dumps(cookies).encode('utf-8'))
         self.set_cookies(driver, cookies)
+        # 这一步不能少
+        driver.get(Urls.index)
         self.set_token(driver.current_url)
         return
 
     # 进行上次未完成的任务
     def do_unfinish_work(self):
+        fakenames = self.db.fakenames
+        fake_one = fakenames.find_one_and_delete(filter={})
+        while fake_one:
+            print(fake_one)
+            self.pipe(fake_one['fakename'], fake_one['chname'])
+            fake_one = fakenames.find_one_and_delete(filter={})
 
-        pass
+    def pipe(self, fakename, chname):
+        # TODO 不止一页，以后要把所有查询到的都添加进数据库
+        # 测试数据，这里找了13个公众号后就不行了，{"base_resp":{"ret":200013,"err_msg":"freq control"}}
+        rep = requests.get(Urls.query_biz.format(random=random.random(), token=Session.token,
+                                                 query=fakename, begin=0, count=5), cookies=Session.cookies, headers=Session.headers)
+        print(rep.text)
 
     # 给Session设置token
     def set_token(self, url):
@@ -130,11 +144,17 @@ def initBizs():
                 while line:
                     idx += 1
                     arr = line.strip().split(' ')
+                    if len(arr) < 2:
+                        arr.append('')
                     fakename = {
-                        'fakename': arr[0],
-                        'chname': arr[1]
+                        # 解决诡异的\ufeff
+                        'fakename': arr[0].encode('utf-8').decode('utf-8-sig').strip(),
+                        'chname': arr[1].encode('utf-8').decode('utf-8-sig').strip()
                     }
-                    fakenames.insert_one(fakename)
+                    fakenames.update_one(
+                        {'fakename': fakename['fakename']},
+                        {"$set": fakename }, True)
+
                     line = fi.readline()
         else:
             print('err')
@@ -149,4 +169,4 @@ def initBizs():
 if __name__ == '__main__':
     print('__main__')
     initBizs()
-    # Spider().main()
+    Spider().main()
