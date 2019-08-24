39f3ea8e5f6e1114b9f45be87da29e5147e65c08
= <sotyoyo@icloud.com>
2019-08-24 13:26:42 +0800

    add 添加公众号list

---
+++ spider/main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import time
+from bs4 import BeautifulSoup
+import requests
+import re
+import shutil
+import os
+import json
+import argparse
+import traceback
+import random
+import math
+import codecs
+import datetime
+import lxml
+
+class Session:
+    token = ''
+    cookies = []
+    # TODO headers User-Agent 定期自动替换
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+
+class Urls:
+    index = 'https://mp.weixin.qq.com'
+    # editor = 'https://mp.weixin.qq.com/cgi-bin/appmsg?t=media/appmsg_edit&action=edit&type=10&isMul=1&isNew=1&share=1&lang=zh_CN&token={token}'
+    query_biz = 'https://mp.weixin.qq.com/cgi-bin/searchbiz?action=search_biz&token={token}&lang=zh_CN&f=json&ajax=1&random={random}&query={query}&begin={begin}&count={count}'
+    query_arti = 'https://mp.weixin.qq.com/cgi-bin/appmsg?token={token}&lang=zh_CN&f=json&%E2%80%A65&action=list_ex&begin={begin}&count={count}&query={query}&fakeid={fakeid}&type=9'
+
+# 主入口
+if __name__ == '__main__':
+    description = u"公众号文章Spider"
+    parser = argparse.ArgumentParser(description=description)
