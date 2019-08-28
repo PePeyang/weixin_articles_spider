ad7328ca71e654e50b3c8020fde0a5dcedca7e19
= <sotyoyo@icloud.com>
2019-08-28 17:55:33 +0800

    remove 无用

--- spider/main.py
+++ spider/main.py
@@ -1,235 +1,268 @@
-# -*- coding: utf-8 -*-
-# TODO 自定义类
-from selenium import webdriver
-import time
-from bs4 import BeautifulSoup
-import requests
-import re
-import os
+# 构造home请求获取 appmsg_token
+# 构造分页请求
+#
+from new_tools.load_list_parse import list_into_dbdata, list_parse
+from new_phone.operate import Operate
+from instance import redis_instance, db_instance, db_loadlist
 import json
-import traceback
-import random
-import math
-import codecs
-import datetime
-import lxml
-import pymongo
+import re
+from time import time
+import datetime as d
+import requests
+from http.cookies import SimpleCookie
+from http import cookiejar
+import urllib.parse as urlparse
 
-class Session:
-    token = ''
-    cookies = []
+class Fakehomeparams:
+    cookies = {
+        'rewardsn': '',
+        'wxuin': '3604431997',
+        'devicetype': 'android-23',
+        'version': '27000634',
+        'lang': 'zh_CN',
+        'pass_ticket': '',
+        'wap_sid2': '',
+        'wxtokenkey': '777',
+    }
 
-    # TODO headers User-Agent 定期自动替换
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+    headers = {
+        'Host': 'mp.weixin.qq.com',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 8.1; PAR-AL00 Build/HUAWEIPAR-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/044304 Mobile Safari/537.36 MicroMessenger/6.7.3.1360(0x26070333) NetType/WIFI Language/zh_CN Process/tools',
+        'x-wechat-uin': '',
+        'x-wechat-key': '',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,en-US;q=0.8',
+        'X-Requested-With': 'com.tencent.mm',
+    }
 
-class Urls:
-    index = 'https://mp.weixin.qq.com'
-    editor = 'https://mp.weixin.qq.com/cgi-bin/appmsg?t=media/appmsg_edit&action=edit&type=10&isMul=1&isNew=1&share=1&lang=zh_CN&token={token}'
-    query_biz = 'https://mp.weixin.qq.com/cgi-bin/searchbiz?action=search_biz&token={token}&lang=zh_CN&f=json&ajax=1&random={random}&query={query}&begin={begin}&count={count}'
-    query_arti = 'https://mp.weixin.qq.com/cgi-bin/appmsg?token={token}&lang=zh_CN&f=json&%E2%80%A65&action=list_ex&begin={begin}&count={count}&query={query}&fakeid={fakeid}&type=9'
-    query_comment = 'https://mp.weixin.qq.com/mp/appmsg_comment?action=getcomment&scene=0&__biz={fakeid}&appmsgid={appmsgid}&idx=1&comment_id={comment_id}&offset={offset}&limit=100&uin=MTcxNzk4OTQwNQ%253D%253D&key={key}&pass_ticket={pass_ticket}&wxtoken=777&devicetype={devicetype}&clientversion=12031a12&appmsg_token={appmsg_token}&x5=0&f=json'
-
-class BaseResp:
-    def __init__(self, strjson):
-        self.data = json.loads(strjson)
-        self.base_resp = self.data['base_resp']
-
-    @property
-    def ret(self):
-        return self.base_resp['ret']
-
-    @property
-    def err_msg(self):
-        return self.base_resp['err_msg']
-
-    @property
-    def is_ok(self):
-        return self.base_resp['ret'] == 0
-
-
-class FakesResp(BaseResp):
-
-    def __init__(self, strjson):
-        super(FakesResp, self).__init__(strjson)
-        print(self.err_msg)
-        self.list = self.data['list'] if self.is_ok else []
-        self.total = self.total = self.data['total'] if self.is_ok else []
+    params = (
+        ('action', 'home'),
+        ('__biz', ''),
+        ('devicetype', 'android-23'),
+        ('version', '27000634'),
+        ('lang', 'zh_CN'),
+        ('nettype', 'WIFI'),
+        ('a8scene', '7'),
+        ('session_us', 'gh_9e26999263b5'),
+        ('pass_ticket', ''),
+        ('wx_header', '1'),
+    )
 
 
+class Fakeloadparams:
+    cookies = {
+        'rewardsn': '',
+        'wxtokenkey': '777',
+        'wxuin': '3604431997',
+        'devicetype': 'android-23',
+        'version': '27000634',
+        'lang': 'zh_CN',
+        'pass_ticket': '',
+        'wap_sid2': '',
+    }
 
-class Spider:
-    def __init__(self):
-        print('Spider init...')
-        self.connect_mongodb()
+    headers = {
+        'Host': 'mp.weixin.qq.com',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 8.1; PAR-AL00 Build/HUAWEIPAR-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/044304 Mobile Safari/537.36 MicroMessenger/6.7.3.1360(0x26070333) NetType/WIFI Language/zh_CN Process/tools',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept': '*/*',
+        # 'Referer': 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzUyMzkwNTQzNQ==&devicetype=android-23&version=27000634&lang=zh_CN&nettype=WIFI&a8scene=7&session_us=gh_9e26999263b5&pass_ticket=%2FwVUpAEtgoBvTouSB3nRI5qyK6t2sGFiihU0qj8IjUsCRWbJixaNVyZ7%2FeO0iFjG&wx_header=1',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,en-US;q=0.8',
+    }
+
+    params = (
+        ('action', 'getmsg'),
+        ('__biz', 'MzUyMzkwNTQzNQ=='),
+        ('f', 'json'),
+        ('offset', '0'),
+        ('count', '10'),
+        ('is_ok', '1'),
+        ('scene', ''),
+        ('uin', '777'),
+        ('key', '777'),
+        ('pass_ticket', ''),
+        ('wxtoken', ''),
+        ('appmsg_token', ''),
+        ('x5', '0'),
+    )
+
+class NORMAL_URLS:
+    home = "https://mp.weixin.qq.com/mp/profile_ext?action=home"
+    getmsg = "https://mp.weixin.qq.com/mp/profile_ext?action=getmsg"
+    article = "https://mp.weixin.qq.com/s?"
+
+def replace_at_index(tup, ix, val):
+    lst = list(tup)
+    lst[ix] = val
+    return tuple(lst)
 
 
-    def __del__(self):
-        print('Spider delete...')
-        self.close_mongodb()
+def send_request():
+    response = requests.get('https://mp.weixin.qq.com/mp/profile_ext',
+                        headers=Fakehomeparams.headers, params=Fakehomeparams.params, cookies=Fakehomeparams.cookies)
+    if response.content.decode().find('失效的验证页面') > 0:
+        print('失效的验证页面')
+    else:
+        login_cookies = requests.utils.dict_from_cookiejar(response.cookies)
+        print('第二次的wap_sid2=' + login_cookies['wap_sid2'])
+        print('pass_ticket=' + login_cookies['pass_ticket'])
+        Fakeloadparams.cookies['pass_ticket'] = login_cookies['pass_ticket']
+        Fakeloadparams.cookies['wap_sid2'] = login_cookies['wap_sid2']
+        Fakeloadparams.params = replace_at_index(
+            Fakeloadparams.params, 9, ('pass_ticket', login_cookies['pass_ticket']))
+        return response.content.decode()
 
-    def main(self):
-        # /usr/bin/chromedriver
-        chrome = input('请输入chromedriver路径 (注意要和自己的chrome版本匹配):').strip()
-        if not chrome:
-            if os.path.isfile('/usr/bin/chromedriver'):
-                chrome = '/usr/bin/chromedriver'
-            else:
-                print('没有合适的chromedriver，程序无法进行！')
-                return
 
-        driver = webdriver.Chrome(executable_path=chrome)
-        print('Succeed to load driver')
-        self.do_weixin_login(driver)
-        self.do_unfinish_work()
-        return
 
-    # 微信首页的登陆
-    def do_weixin_login(self, driver):
-        if not driver:
-            print('Are you kidding me ？没有driver玩毛线')
-            return
-        base_dir = os.path.dirname(__file__)
-        cookies_path = os.path.join(base_dir, 'caches/cookies.json')
-        cookies = json.load(open(cookies_path, 'rb')
-                            ) if os.path.isfile(cookies_path) else []
-        driver.get(Urls.index)
-        if not cookies:
-            # TODO 自动输入账号密码 给出二维码然后自动登陆 只需要自己扫个码
-            # ANCHOR cookie一般一天就到期
-            input("请先手动登录, 完成后按回车继续:")
-            cookies = driver.get_cookies()
-            open(cookies_path, 'wb').write(json.dumps(cookies).encode('utf-8'))
-        self.set_cookies(driver, cookies)
-        # 这一步不能少
-        driver.get(Urls.index)
-        self.set_token(driver.current_url)
-        return
+def build_home_request_1(data):
+    # print(data)
+    cookie = SimpleCookie()
+    cookie.load(data['REQUEST_COOKIE'])
+    cookies = {}
+    cookies = {i.key: i.value for i in cookie.values()}
+    Fakehomeparams.cookies['wxuin'] = cookies['wxuin']
+    # Fakehomeparams.cookies['version'] = cookies['version']
+    Fakehomeparams.cookies['pass_ticket'] = cookies['pass_ticket']
+    Fakehomeparams.cookies['wap_sid2'] = cookies['wap_sid2']
+    Fakehomeparams.params = replace_at_index(
+        Fakehomeparams.params, 8, ('pass_ticket', cookies['pass_ticket']))
 
-    # 进行上次未完成的任务
-    def do_unfinish_work(self):
-        fakenames = self.db.fakenames
-        fake_one = fakenames.find_one_and_delete(filter={})
-        while fake_one:
-            print(fake_one)
-            self.pipe(fake_one['fakename'], fake_one['chname'])
-            time.sleep(6)
-            fake_one = fakenames.find_one_and_delete(filter={})
+def build_home_request_2(data):
+    # print(data)
+    Fakehomeparams.headers['x-wechat-uin'] = data['REQUEST_HEADERS']['X-WECHAT-UIN']
+    Fakehomeparams.headers['x-wechat-key'] = data['REQUEST_HEADERS']['X-WECHAT-KEY']
+    biz = urlparse.parse_qs(data['REQUEST_DATA'])['__biz'][0]
+    print('X-WECHAT-UIN=' + data['REQUEST_HEADERS']['X-WECHAT-UIN'])
+    print('X-WECHAT-KEY=' + data['REQUEST_HEADERS']['X-WECHAT-KEY'])
+    print('biz=' + biz)
+    Fakehomeparams.params = replace_at_index(
+        Fakehomeparams.params, 1, ('__biz', biz))
 
-    def pipe(self, fakename, chname):
-        # TODO 不止一页，以后要把所有查询到的都添加进数据库
-        # time.sleep(0) 13个公众号 {"base_resp":{"ret":200013,"err_msg":"freq control"}}
-        # time.sleep(3) 23个
-        # 大概访问一百个后，账号直接给我ban了。。。 -..= 目前看来是三个小时左右
-        rep = requests.get(Urls.query_biz.format(random=random.random(), token=Session.token,
-                                                 query=fakename, begin=0, count=5),
-                                                 cookies=Session.cookies, headers=Session.headers)
 
-        response = FakesResp(rep.text)
 
-        item = {
-            'list': response.list,
-            'total': response.total,
-            'fakename': fakename,
-            'chname': chname
-        }
+def build_load_request(appmsg_token):
+    print(appmsg_token)
 
-        print(item)
-        if not response.is_ok:
-            return
-        else:
-            bizs = self.db.bizs
+    Fakeloadparams.params = replace_at_index(
+        Fakeloadparams.params, 1, ('__biz', Fakehomeparams.params[1][1]))
+
+    Fakeloadparams.params = replace_at_index(
+        Fakeloadparams.params, 11, ('appmsg_token', appmsg_token))
+
+    print(Fakeloadparams.headers)
+    print(Fakeloadparams.cookies)
+    print(Fakeloadparams.params)
+
+
+def save_list_to_db(list_db):
+    # TODO fix
+    db_loadlist.insert_many(list_db)
+    pass
+
+def loop_request_load():
+    import time
+    print(d.datetime.now().strftime("%Y.%m.%d-%H:%M:%S"))
+    idx=0
+    items_len=0
+    offset=0
+    # return
+    # 循环机制改变一下 需要捕捉到所有的真实错误
+    # ANCHOR 测试结果 一天最多请求 1000次
+    while True:
+        try:
             try:
-                bizs.update_one(
-                    filter={'fakename': item['fakename']},
-                    update={"$set": item},
-                    upsert=True
+                response = requests.get(
+                    'https://mp.weixin.qq.com/mp/profile_ext',
+                    headers=Fakeloadparams.headers,
+                    params=Fakeloadparams.params,
+                    cookies=Fakeloadparams.cookies
                 )
-            except Exception as dberr:
-                print(dberr)
+            except Exception as request_err:
+                print(request_err)
+                raise Exception('HUMAN_ERROR: 请求出错了')
 
+            idx+=1
+            offset+=10
 
+            Fakeloadparams.params = replace_at_index(
+                Fakeloadparams.params, 3, ('offset', offset))
 
-    # 给Session设置token
-    def set_token(self, url):
-        if 'token' not in url:
-            raise Exception(f"当前登录的https://mp.weixin.qq.com没有Token")
-        Session.token = re.findall(r'token=(\w+)', url)[0]
-        Session.headers['Referer'] = Urls.editor.format(token=Session.token)
-        Session.headers['Host'] = "mp.weixin.qq.com"
-        Session.headers['X-Requested-With'] = "XMLHttpRequest"
-        Session.headers['Connection'] = "keep-alive"
+            list_parse_res = list_parse(eval(response.text))
+            print(type(list_parse_res))
+            if type(list_parse_res['ret']) == type(0):
+                print(list_parse_res)
+                raise Exception('HUMAN_ERROR: 这条数据parse失败了')
+            list_db_data = list_into_dbdata(list_parse_res)
+            if not list_db_data:
+                raise Exception('HUMAN_ERROR: 后面无数据了')
 
-
-
-    # 给Session设置cookie
-    def set_cookies(self, driver, cookies):
-        Session.cookies = {}
-        for item in cookies:
-            driver.add_cookie(item)
-            Session.cookies[item['name']] = item['value']
-
-    def connect_mongodb(self):
-        print('weixindb connecting...')
-        try:
-            client = pymongo.MongoClient("localhost", 27017)
-            self.client = client
-            self.db = client.weixindb
-            print('weixindb connected')
+            try:
+                save_list_to_db(list_db_data)
+            except Exception as db_err:
+                print(db_err)
+                raise Exception('HUMAN_ERROR: 数据库插入出错了')
+            items_len += len(list_db_data)
+            print('成功处理第 {} 个load请求，当前 offset= {} items_len= {}'.format(
+                idx, offset, items_len))
+            time.sleep(3)
         except Exception as err:
-            print('mongodb connect with error:')
+            print('失败处理第 {} 个load请求，当前 offset= {} items_len= {}'.format(
+                idx, offset, items_len))
             print(err)
+            if str(err).find('无数据') > 0:
+                break
+    print(d.datetime.now().strftime("%Y.%m.%d-%H:%M:%S"))
 
 
-    def close_mongodb(self):
-        print('mongodb closing...')
-        try:
-            self.client.close()
-            print('mongodb closed')
-        except Exception as err:
-            print('mongodb closed with error:')
-            print(err)
+def operate_phone(client):
+    client.home_click()
+    # client.search_text()
+    # client.tab_click()
+    # client.enter_into_gzh()
 
 
-# for test
-def initBizs():
-    try:
-        client = pymongo.MongoClient("localhost", 27017)
-        db = client.weixindb
-        fakenames = db.fakenames
-        base_dir = os.path.dirname(__file__)
-        ac = os.path.join(base_dir, 'fakes/fakenames.txt')
-        idx = 0
-        if os.path.isfile(ac):
-            with open(ac, 'rt') as fi:
-                line = fi.readline()
-                while line:
-                    idx += 1
-                    arr = line.strip().split(' ')
-                    if len(arr) < 2:
-                        arr.append('')
-                    fakename = {
-                        # 解决诡异的\ufeff
-                        'fakename': arr[0].encode('utf-8').decode('utf-8-sig').strip(),
-                        'chname': arr[1].encode('utf-8').decode('utf-8-sig').strip()
-                    }
-                    fakenames.update_one(
-                        filter={'fakename': fakename['fakename']},
-                        update={"$set": fakename },
-                        upsert=True
-                    )
-
-                    line = fi.readline()
-        else:
-            print('err')
-
-        client.close()
-    except Exception as err:
-        print('mongodb connect with error:')
-        print(err)
-
-
-# 主入口
 if __name__ == '__main__':
     print('__main__')
-    initBizs()
-    Spider().main()
+    client = Operate('苏州青舞舞蹈艺术')
+    operate_phone(client)
+
+
+    # 一组一组拿出来
+    # keys = redis_instance.keys('*_REQUEST')
+    # # 1.json
+    # geticon_key = str(keys[0], encoding="utf-8")
+    # # 2.json
+    # getappmsgext_key = str(keys[1], encoding="utf-8")
+    # geticon_value = redis_instance.get(geticon_key)
+    # getappmsgext_value = redis_instance.get(getappmsgext_key)
+    # geticon_value = json.loads(geticon_value.decode())
+    # getappmsgext_value = json.loads(getappmsgext_value.decode())
+    # # print(geticon_value)
+    # # print(getappmsgext_value
+    # build_home_request_1(geticon_value)
+    # build_home_request_2(getappmsgext_value)
+    # content = send_request()
+    # if not content:
+    #     print('无效content')
+    #     content = ''
+
+
+    # if content.find('操作频繁') > 0:
+    #     print('HUMAN_ERROR: 操作频繁 限制24小时 请更换微信')
+
+
+    # pat = re.compile(r'window.appmsg_token = "(.*?)"')
+    # appmsg_tokens = pat.findall(content, pos=0)
+
+    # for m in appmsg_tokens:
+    #     build_load_request(m)
+    #     loop_request_load()
+
+
+
+
