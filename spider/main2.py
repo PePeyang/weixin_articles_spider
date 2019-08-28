15456816e64e2d6de1531d296bb2641ee60a4dbe
= <sotyoyo@icloud.com>
2019-08-28 13:04:48 +0800

    fix

--- spider/main2.py
+++ spider/main2.py
@@ -108,8 +108,8 @@ def send_request():
         print('失效的验证页面')
     else:
         login_cookies = requests.utils.dict_from_cookiejar(response.cookies)
-        print(login_cookies['wap_sid2'])
-        print(login_cookies['pass_ticket'])
+        print('第二次的wap_sid2=' + login_cookies['wap_sid2'])
+        print('pass_ticket=' + login_cookies['pass_ticket'])
         Fakeloadparams.cookies['pass_ticket'] = login_cookies['pass_ticket']
         Fakeloadparams.cookies['wap_sid2'] = login_cookies['wap_sid2']
         Fakeloadparams.params = replace_at_index(
@@ -136,6 +136,9 @@ def build_home_request_2(data):
     Fakehomeparams.headers['x-wechat-uin'] = data['REQUEST_HEADERS']['X-WECHAT-UIN']
     Fakehomeparams.headers['x-wechat-key'] = data['REQUEST_HEADERS']['X-WECHAT-KEY']
     biz = urlparse.parse_qs(data['REQUEST_DATA'])['__biz'][0]
+    print('X-WECHAT-UIN=' + data['REQUEST_HEADERS']['X-WECHAT-UIN'])
+    print('X-WECHAT-KEY=' + data['REQUEST_HEADERS']['X-WECHAT-KEY'])
+    print('biz=' + biz)
     Fakehomeparams.params = replace_at_index(
         Fakehomeparams.params, 1, ('__biz', biz))
 
@@ -150,9 +153,9 @@ def build_load_request(appmsg_token):
     Fakeloadparams.params = replace_at_index(
         Fakeloadparams.params, 11, ('appmsg_token', appmsg_token))
 
-    # print(Fakeloadparams.headers)
-    # print(Fakeloadparams.cookies)
-    # print(Fakeloadparams.params)
+    print(Fakeloadparams.headers)
+    print(Fakeloadparams.cookies)
+    print(Fakeloadparams.params)
 
 
 def save_list_to_db(list_db):
@@ -164,31 +167,55 @@ def loop_request_load():
     import time
     print(d.datetime.now().strftime("%Y.%m.%d-%H:%M:%S"))
     idx=0
+    items_len=0
     offset=0
+    # return
     # 循环机制改变一下 需要捕捉到所有的真实错误
+    # ANCHOR 测试结果 一天最多请求 1000次
     while True:
         try:
-            response = requests.get(
-                'https://mp.weixin.qq.com/mp/profile_ext',
-                headers=Fakeloadparams.headers,
-                params=Fakeloadparams.params,
-                cookies=Fakeloadparams.cookies
-            )
-            Fakeloadparams.params = replace_at_index(
-                Fakeloadparams.params, 3, ('offset', offset))
+            try:
+                response = requests.get(
+                    'https://mp.weixin.qq.com/mp/profile_ext',
+                    headers=Fakeloadparams.headers,
+                    params=Fakeloadparams.params,
+                    cookies=Fakeloadparams.cookies
+                )
+            except Exception as request_err:
+                print(request_err)
+                raise Exception('HUMAN_ERROR: 请求出错了')
+
             idx+=1
             offset+=10
-            test = list_parse(eval(response.text))
-            list_db_data = list_into_dbdata(test)
+
+            Fakeloadparams.params = replace_at_index(
+                Fakeloadparams.params, 3, ('offset', offset))
+
+            list_parse_res = list_parse(eval(response.text))
+            print(type(list_parse_res))
+            if type(list_parse_res['ret']) == type(0):
+                print(list_parse_res)
+                raise Exception('HUMAN_ERROR: 这条数据parse失败了')
+            list_db_data = list_into_dbdata(list_parse_res)
             if not list_db_data:
-                raise Exception('后面无数据了')
-            save_list_to_db(list_db_data)
-            print('现在正在处理第 {} 个load请求，每次请求10条，当前 offset= {}'.format(idx, offset))
-            time.sleep(2)
+                raise Exception('HUMAN_ERROR: 后面无数据了')
+
+            try:
+                save_list_to_db(list_db_data)
+            except Exception as db_err:
+                print(db_err)
+                raise Exception('HUMAN_ERROR: 数据库插入出错了')
+            items_len += len(list_db_data)
+            print('成功处理第 {} 个load请求，当前 offset= {} items_len= {}'.format(
+                idx, offset, items_len))
+            time.sleep(3)
         except Exception as err:
-            print('发现异常，当前一共处理了 {} 个load请求'.format(idx) )
+            print('失败处理第 {} 个load请求，当前 offset= {} items_len= {}'.format(
+                idx, offset, items_len))
             print(err)
-            print(d.datetime.now().strftime("%Y.%m.%d-%H:%M:%S"))
+            if str(err).find('无数据') > 0:
+                break
+    print(d.datetime.now().strftime("%Y.%m.%d-%H:%M:%S"))
 
 
 
@@ -211,10 +238,17 @@ if __name__ == '__main__':
     build_home_request_2(getappmsgext_value)
     content = send_request()
     if not content:
+        print('无效content')
         content = ''
+
+
+    if content.find('操作频繁') > 0:
+        print('HUMAN_ERROR: 操作频繁 限制24小时 请更换微信')
+
 
     pat = re.compile(r'window.appmsg_token = "(.*?)"')
     appmsg_tokens = pat.findall(content, pos=0)
+
     for m in appmsg_tokens:
         build_load_request(m)
         loop_request_load()
--- spider/new_tools/load_list_parse.py
+++ spider/new_tools/load_list_parse.py
@@ -6,19 +6,23 @@
 4. 成功有内容的return load_list
 """
 def list_parse(load_res_dict):
-    # print(load_res_dict['ret'])
+    print(type(load_res_dict['general_msg_list']))
+    load_list = {
+        'list': []
+    }
     if not load_res_dict['ret'] == 0:
         return load_res_dict
     elif not type(load_res_dict['general_msg_list']) == type(str('s')):
         return load_res_dict
     else:
         load_list = eval(load_res_dict['general_msg_list'])
-        if(len(load_list['list']) == 0):
+        if (not isinstance(load_list['list'], list)) or (len(load_list['list']) == 0):
             return load_res_dict
         else:
             print('Success')
             # print(load_list)
-            return load_list
+
+    return load_list
 
 
 """
@@ -28,25 +32,36 @@ return list
 TODO 存入biz 公众号名字 发表时间
 """
 def list_into_dbdata(obj):
-    list_lo = obj['list']
-    list_db = list()
-    if len(list_lo) > 0:
-        for big_li in list_lo:
-            big_item = {}
-            for b_key, b_value in big_li['app_msg_ext_info'].items():
-                if b_key == 'content_url' or b_key == 'cover' or b_key == 'source_url':
-                    b_value = b_value.replace('\\', '')
-                if (b_key == 'multi_app_msg_item_list') and (len(b_value) > 0):
-                    for small_li in b_value:
-                        # print(small_li)
-                        small_item = {}
-                        for s_key, s_value in small_li.items():
-                            if s_key == 'content_url' or s_key == 'cover' or s_key == 'source_url':
-                                s_value = s_value.replace('\\', '')
-                            small_item[s_key] = s_value
-                        list_db.append(small_item)
-                else:
-                    big_item[b_key] = b_value
-            list_db.append(big_item)
-        # print(len(list_db))
-        return list_db
+    try:
+        list_lo = obj['list']
+        list_db = list()
+        if not isinstance(list_lo, list):
+            print('list_lo 不是list类型')
+            print(list_lo)
+            return
+
+        if len(list_lo) > 0:
+            for big_li in list_lo:
+                big_item = {}
+                for b_key, b_value in big_li['app_msg_ext_info'].items():
+                    if b_key == 'content_url' or b_key == 'cover' or b_key == 'source_url':
+                        b_value = b_value.replace('\\', '')
+                    if (b_key == 'multi_app_msg_item_list') and (len(b_value) > 0):
+                        for small_li in b_value:
+                            # print(small_li)
+                            small_item = {}
+                            for s_key, s_value in small_li.items():
+                                if s_key == 'content_url' or s_key == 'cover' or s_key == 'source_url':
+                                    s_value = s_value.replace('\\', '')
+                                small_item[s_key] = s_value
+                            list_db.append(small_item)
+                    else:
+                        big_item[b_key] = b_value
+                list_db.append(big_item)
+            # print(len(list_db))
+            return list_db
+        else:
+            return None
+    except Exception as err:
+        print(err)
+        print('list_into_dbdata错误')
