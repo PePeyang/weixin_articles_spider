a5c31f47d8f41f70271d522b8ba4609fde567f3b
= <sotyoyo@icloud.com>
2019-08-31 22:03:09 +0800

    完成安卓队列运行和通知http

--- android/process_adb.py
+++ android/process_adb.py
@@ -1,15 +1,58 @@
 # -*- coding:utf-8 -*-
+from bson.objectid import ObjectId
+import redis
 import datetime
 import time
 import sys
 sys.path.append("../")  # 为了引入instance
 from instance import mongo_instance  # weixindb
-from bson.objectid import ObjectId
-
+r = redis.Redis()
 
 def adb_entry(android_queue):
+    # 每隔一分钟去队列检查下是否有任务在running 没有的话就搞一个变成runnning
 
-    pass
+    while True:
+        t = datetime.datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
+        tasks = android_queue.pickAll()
+        if not tasks:
+            print('- {} 没有任何任务，安卓adb啥事也不用干'.format(t))
+        else:
+            runnning_tasks = pick_running(tasks)
+            if not runnning_tasks:
+                print('- {} 即将添加任务至安卓运行队列'.format(t))
+                # 如果没有任何进行中的任务
+                # 1. 将第一个任务设为进行中 存入数据库
+                # 2. 通知代理，你有活要干啦！ 并且删除他
+                try:
+                    print('- {} 即将开始安卓任务'.format(t))
+                    set_task_running(tasks[0])
+                    print('- {} 即将通知anyproxy'.format(t))
+                    notify_http_proxy(str(tasks[0]['_id']))
+                except Exception as e:
+                    print(' - 31行 这里出错了')
+                    print(e)
+            else:
+                print('- {} 已经有运行中的任务了哦 _id是 {}'.format(t, str(runnning_tasks[0]['_id'])))
 
-def suber():
-    pass
+
+        time.sleep(10)
+
+
+def pick_running(tasks):
+    runnning_tasks = []
+    for task in tasks:
+        # 同一时间只能有一个running的
+        if task['task_status'] == 'running':
+            runnning_tasks.append(task)
+            break
+    return runnning_tasks
+
+
+def set_task_running(task):
+    task['task_status'] = 'running'
+    mongo_instance.tasks.find_and_modify(
+        query={'_id': task['_id']}, update={'$set': {'task_status': 'runnning'}})
+
+
+def notify_http_proxy(taskid):
+    r.publish('there_is_a_http', '__taskid_' + str(taskid))
