57479a70b86c2b85c5c20e7d028e6bde2d222e41
= <sotyoyo@icloud.com>
2019-09-04 09:41:37 +0800

    完成scrapy循环运作

--- android_scrapy/main.py
+++ android_scrapy/main.py
@@ -1,57 +1,35 @@
-import os
-import sys
-from threading import Thread
-import datetime
-from process_scrapy import start_scrapy_loads
-from scrapy import cmdline
+import scrapy
+from android_scrapy.spiders.loadSpider import LoadSpider
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor
+from twisted.internet.task import deferLater
+from instance import redis_instance
+import time
 
-class SCRAPY_THREAD (Thread):
-    def __init__(self):
-        Thread.__init__(self)
-
-    def run(self):
-        try:
-            start_scrapy_loads()
-        except BaseException as be:
-            print(be)
-            # self.run()
-
-    def join(self):
-        Thread.join(self)
+def sleep(self, *args, seconds):
+    """Non blocking sleep callback"""
+    return deferLater(reactor, seconds, lambda: None)
 
 
-if __name__ == '__main__':
-    # signal.signal(signal.SIGINT, quit)
-    # signal.signal(signal.SIGTERM, quit)
-
-    # 启动 SCRAPY_THREAD
-    bftime = datetime.datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
-    print('- {} SCRAPY_THREAD 启动中...'.format(bftime))
-    t = SCRAPY_THREAD()
-    t.start()
-    aftime = datetime.datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
-    print('- {} SCRAPY_THREAD 已启动'.format(aftime))
-    t.join()
-    fftime = datetime.datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
-    print('- {} SCRAPY_THREAD 已结束'.format(fftime))
+process = CrawlerProcess(get_project_settings())
 
 
-# os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'myproject.settings')
-# settings.overrides['LOG_ENABLED'] = False  # avoid log noise
+def _crawl(result, spider):
+    deferred = process.crawl(spider)
+    deferred.addCallback(lambda results: print(
+        '稍等。1秒后会自动重启...'))
+    deferred.addCallback(sleep, seconds=1)
+    deferred.addCallback(_crawl, spider)
+    return deferred
 
 
-# def item_passed(item):
-#     print "Just scraped item:", item
-
-
-# dispatcher.connect(item_passed, signal=signals.item_passed)
-
-# crawler = CrawlerThread()
-# print "Starting crawler thread..."
-# crawler.start()
-
-# crawler.crawl('LoadSpider')  # blocking call
-# print "Crawling anotherdomain.com..."
-# crawler.crawl('anotherdomain.com')  # blocking call
-# print "Stopping crawler thread..."
-# crawler.stop()
+while True:
+    httpid_b = redis_instance.get('__running_http_')
+    if httpid_b:
+        print('检测到了新的的http')
+        _crawl(None, LoadSpider)
+        process.start()
+    else:
+        print('没有成功捕获到的http')
+    time.sleep(30)
