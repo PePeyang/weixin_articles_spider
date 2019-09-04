946cda4cb4244c6d48e83b7201a67c3b26e88102
= <sotyoyo@icloud.com>
2019-09-04 13:23:07 +0800

    fix

--- android/main.py
+++ android/main.py
@@ -7,19 +7,8 @@ from process_listen import listen_task_entry
 from threading import Thread
 import datetime
 
-class SUBER_THREAD (Thread):
-    def __init__(self, queue):
-        self.queue = queue
-        Thread.__init__(self)
-
-    def run(self):
-        suber_entry(self.queue)
-
-    def join(self):
-        Thread.join(self)
-
 class ADB_THREAD (Thread):
-    def __init__(self, queue):
+    def __init__(self):
         Thread.__init__(self)
 
     def run(self):
@@ -56,14 +45,6 @@ if __name__ == '__main__':
     t_listen.start()
     aftime = datetime.datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
     print('- {} LITEN_TASK_THREAD 已启动'.format(aftime))
-
-    # # 启动 SUBER_THREAD
-    # bftime = datetime.datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
-    # print('- {} SUBER_THREAD 启动中...'.format(bftime))
-    # t1 = SUBER_THREAD()
-    # t1.start()
-    # aftime = datetime.datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
-    # print('- {} SUBER_THREAD 已启动'.format(aftime))
 
     # 启动 ADB_THREAD
     bftime = datetime.datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
