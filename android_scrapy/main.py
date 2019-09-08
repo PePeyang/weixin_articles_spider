4583e8b3ec3955e8cd866082c81d6b2be82f63c1
= <sotyoyo@icloud.com>
2019-09-08 21:12:35 +0800

    麻烦大佬看下这个提交

--- android_scrapy/main.py
+++ android_scrapy/main.py
@@ -9,6 +9,15 @@ from instance import mongo_instance, redis_instance
 import time
 import sys, os
 
+"""
+ANCHOR
+这里很丑陋，实在是太菜了，没法解决一些问题
+1. 首先scrapy应当在项目外部启动，。。不会 我把这个文件搬出去以后 运行loadSpider 会卡在一个地方不继续了。
+2. 这里的instance应当使用全局的instance。。。不会
+3. 其实目前这里重启的是loadspider 但是应该重启的是整个process 不知道能不能做到。。。
+4. 其实这里最好是 adb在操作以后 调用这个loadspider一次，再去调用articlespider一次。。。不会
+这些问题没有解决，其实需要一次性调整一下项目结构就可以解决，但是我怎么调都一堆问题，还请大家帮帮忙 提提意见
+"""
 
 def sleep(self, *args, seconds):
     """Non blocking sleep callback"""
@@ -28,15 +37,20 @@ def _crawl(result, spider):
 
 suber = redis_instance.pubsub()
 suber.subscribe('there_is_a_http')
-while True:
-    res = suber.parse_response()
-    httpid = redis_instance.get('__running_http_')
-    if httpid:
-        # redis_instance.delete('__running_http_')
-        print('检测到了新的的http')
-        _crawl(None, LoadSpider)
 
-        process.start()
-    else:
-        print('没有成功捕获到新的http')
-        # _crawl(None, ArticleSpider)
+try:
+    while True:
+        res = suber.parse_response()
+        httpid = redis_instance.get('__running_http_')
+        if httpid:
+            # redis_instance.delete('__running_http_')
+            print('检测到了新的的http')
+            _crawl(None, LoadSpider)
+            process.start()
+        else:
+            print('没有成功捕获到新的http')
+            # _crawl(None, ArticleSpider)
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("优雅的手动停止...")
+    sys.exit(1)
